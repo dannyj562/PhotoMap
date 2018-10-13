f23db5e5fc4167dd3df960f77217eac4c5b44229
dannyj562 <riveradanny51@gmail.com>
2018-10-13 13:24:35 -0700

    Add MapKit and set up Map View

---
+++ Photo Map/PhotoMapViewController.swift
@@ -0,0 +1,46 @@
+//
+//  PhotoMapViewController.swift
+//  Photo Map
+//
+//  Created by Nicholas Aiwazian on 10/15/15.
+//  Copyright © 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+import AddressBookUI
+import MessageUI
+
+class PhotoMapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initMap()
+        // Do any additional setup after loading the view.
+    }
+    
+    private func initMap() {
+        let region = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1))
+        self.mapView.setRegion(region, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
